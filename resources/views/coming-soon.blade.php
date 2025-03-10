e57e63da8e9422188c8b94f7e29a1f076f456fd2
newtonjob <jobnewton3@gmail.com>
2025-03-10 12:52:12 +0100

    Satisfy sam

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -212,8 +212,8 @@
                             <div class="swiper-wrapper">
                                 <!-- Item -->
                                 <div class="swiper-slide">
-                                    <h6 class="from-start mb-lg-4" style="font-size:calc(1.3875rem + 1.65vw);font-weight:200;line-height:1.3">WELCOME TO</h6>
-                                    <h2 class="display-1 from-start mb-lg-4">LEBARA NIGERIA</h2>
+                                    <h6 class="from-start mb-lg-4 lh-1" style="font-size:calc(1.3875rem + 1.65vw);font-weight:200;line-height:1.3">WELCOME TO</h6>
+                                    <h2 class="display-1 from-start mb-lg-4 lh-1">LEBARA NIGERIA</h2>
                                     {{--<div class="from-end">
                                         <p class="fs-xl text-white pb-2 mb-lg-5">
                                             Subscribe to get the insider gist of our launch and other premium offers.
@@ -222,8 +222,8 @@
                                 </div>
 
                                 <div class="swiper-slide">
-                                    <h6 class="from-start mb-lg-4" style="font-size:calc(1.3875rem + 1.65vw);font-weight:200 !important;line-height:1.3">WE OFFER</h6>
-                                    <h2 class="display-1 from-start mb-lg-4">Reliable. Affordable and Premium service.</h2>
+                                    <h6 class="from-start mb-lg-4 lh-1" style="font-size:calc(1.3875rem + 1.65vw);font-weight:200 !important;line-height:1.3">WE OFFER</h6>
+                                    <h2 class="display-1 from-start mb-lg-4 lh-1">Reliable. Affordable and Premium service.</h2>
                                 </div>
                             </div>
 
