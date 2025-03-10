53b5c5a3969d4b90b30da702175f487d69cc39d7
newtonjob <jobnewton3@gmail.com>
2025-03-10 13:45:37 +0100

    Update contact

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,5 +1,21 @@
 <x-layouts.site>
-    <section class="bg-secondary pt-1">
+    <div class="jarallax d-none d-md-block" data-jarallax="" data-speed="0.35">
+        <span class="position-absolute top-0 start-0 w-100 h-100 bg-gradient-primary-translucent"></span>
+
+        <div class="d-none d-xxl-block" style="height: 300px;"></div>
+        <div class="d-none d-md-block d-xxl-none" style="height: 300px;"></div>
+        <div id="jarallax-container-0" class="jarallax-container" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden; z-index: -100; clip-path: polygon(0px 0px, 100% 0px, 100% 100%, 0px 100%);"><div class="jarallax-img" style="background-image: url(&quot;assets/img/about/cover.jpg&quot;); object-fit: cover; object-position: 50% 50%; max-width: none; position: fixed; top: 0px; left: 0px; width: 1066px; height: 837.3px; overflow: hidden; pointer-events: none; transform-style: preserve-3d; backface-visibility: hidden; margin-top: 77.35px; transform: translate3d(0px, -50.75px, 0px);" data-jarallax-original-styles="background-image: url('assets/img/about/cover.jpg');"></div></div></div>
+
+    <nav class="container py-4 mb-2 my-lg-3" aria-label="breadcrumb">
+        <ol class="breadcrumb mb-0">
+            <li class="breadcrumb-item">
+                <a href="/"><i class="bx bx-home-alt fs-lg me-1"></i>Home</a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">Product/Service</li>
+        </ol>
+    </nav>
+
+    <section class="bg-secondary pt-1 d-none">
         <div class="position-relative py-lg-4 py-xl-5">
 
             <!-- Swiper tabs -->
