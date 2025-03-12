81d56e746bd89148e74ebee76bc8cbc4674ecfe9
newtonjob <jobnewton3@gmail.com>
2025-03-12 13:55:29 +0100

    Add content to coming soon

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -42,6 +42,7 @@
 
     <!-- Main Theme Styles + Bootstrap -->
     <link rel="stylesheet" media="screen" href="/assets/css/theme.min.css?v=1">
+    <link rel="stylesheet" media="screen" href="/assets/css/colors.css">
 
     <!-- Page loading styles -->
     <style>
@@ -281,6 +282,52 @@
                 </div>
             </div>
         </div>
+    </section>
+
+    @if(false)
+        <section class="py-lg-4 py-xl-5 overflow-hidden bg-blue-50 d-none">
+            <div class="container mt-md-4">
+                <div class="row">
+                    <div class="col-md-5 text-center text-md-start pb-5">
+                        <h1 class="mb-4 text-chambray">We are Lebara</h1>
+                        <p class="fs-lg pb-lg-3 mb-4 text-chambray">
+                            For 24 years, Lebara Group has been a trusted provider of affordable, reliable, and high-quality mobile services across three continents and nine countries. Now, it's Africa’s turn to experience the full benefits of being part of the Lebara family and we will be starting with Nigeria.
+                        </p>
+
+                        <p class="fs-lg pb-lg-3 mb-4 text-chambray">
+                            Beyond top-tier telecommunications, we have forged strategic partnerships to offer financial services designed to empower the creative industry. Through microloans, device funding, free internet hubs, and culture-shaping events, we are committed to driving innovation, inclusion, and economic growth.
+                        </p>
+                        <p class="fs-lg pb-lg-3 mb-4 text-chambray">
+                            Our mission is clear; connecting people, supporting creativity, and shaping the future. We’re ready. Are you?
+                        </p>
+                    </div>
+                    <div class="col-xl-6 col-md-7 offset-xl-1 pb-4 pb-sm-3 pb-lg-0 mb-4 mb-sm-5 mb-lg-0">
+                        <img src="https://www.lebara.nl/content/dam/lebara/markets/nl/nl/assets/images/prepaid/PhoneAppNL_LightBlue%201-min.png/jcr:content/renditions/extra-large-w.webp" alt="Image">
+                    </div>
+                </div>
+            </div>
+        </section>
+    @endif
+
+    <section class="py-5 overflow-hidden bg-blue-50">
+        <section class="container pb-5 mb-md-2 mb-lg-4">
+            <h1 class="pb-4 text-chambray">We are Lebara</h1>
+            <div class="row pt-2 pt-md-3">
+                <div class="col-md-6">
+                    <p class="fs-lg pe-lg-4 mb-4">
+                        For 24 years, Lebara Group has been a trusted provider of affordable, reliable, and high-quality mobile services across three continents and nine countries. Now, it's Africa’s turn to experience the full benefits of being part of the Lebara family and we will be starting with Nigeria.
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <p class="fs-lg ps-lg-4 mb-1 mb-lg-4">
+                        Beyond top-tier telecommunications, we have forged strategic partnerships to offer financial services designed to empower the creative industry.
+                        Through microloans, device funding, free internet hubs, and culture-shaping events, we are committed to driving innovation, inclusion, and economic growth.
+
+                        Our mission is clear; connecting people, supporting creativity, and shaping the future. We’re ready. Are you?
+                    </p>
+                </div>
+            </div>
+        </section>
     </section>
 </main>
 
