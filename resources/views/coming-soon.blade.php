e50d69921c325e6b789cb62c0934be1eafac9998
newtonjob <jobnewton3@gmail.com>
2025-03-13 20:51:33 +0100

    Add Google Analytics

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -149,6 +149,18 @@
             };
         })();
     </script>
+
+    @production
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9GY618XQP2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-9GY618XQP2');
+        </script>
+    @endproduction
 </head>
 <body >
 <!-- Page loading spinner -->
