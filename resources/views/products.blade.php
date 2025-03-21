10c1d0dfc8116fcdc6095b19f3bfaf66f584dbc5
newtonjob <jobnewton3@gmail.com>
2025-03-21 10:20:22 +0100

    Make image bigger

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -133,7 +133,7 @@
 
                     <!-- Image -->
                     <div class="mx-md-n5 mb-md-n5 text-center p-5">
-                        <img src="{{ asset('sim.png') }}" class="w-lg-75" alt="Image">
+                        <img src="{{ asset('sim.png') }}" alt="Image">
                     </div>
 
                     <!-- Items row -->
