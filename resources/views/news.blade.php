58dd5cd140731a477f4735423e134cacf6f7e19f
newtonjob <jobnewton3@gmail.com>
2025-05-19 15:06:53 +0100

    Add different headers for sections

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -2,9 +2,11 @@
     <div class="jarallax d-none d-md-block" data-jarallax="" data-speed="0.35">
         <span class="position-absolute top-0 start-0 w-100 h-100 bg-gradient-primary-translucent"></span>
 
-        <div class="d-none d-xxl-block" style="height: 300px;"></div>
-        <div class="d-none d-md-block d-xxl-none" style="height: 300px;"></div>
-        <div id="jarallax-container-0" class="jarallax-container" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden; z-index: -100; clip-path: polygon(0px 0px, 100% 0px, 100% 100%, 0px 100%);"><div class="jarallax-img" style="background-image: url(&quot;assets/img/about/cover.jpg&quot;); object-fit: cover; object-position: 50% 50%; max-width: none; position: fixed; top: 0px; left: 0px; width: 1066px; height: 837.3px; overflow: hidden; pointer-events: none; transform-style: preserve-3d; backface-visibility: hidden; margin-top: 77.35px; transform: translate3d(0px, -50.75px, 0px);" data-jarallax-original-styles="background-image: url('assets/img/about/cover.jpg');"></div></div></div>
+        <div class="d-none d-xxl-block" style="height: 600px;"></div>
+        <div class="d-none d-md-block d-xxl-none" style="height: 600px;"></div>
+        <div id="jarallax-container-0" class="jarallax-container" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden; z-index: -100; clip-path: polygon(0px 0px, 100% 0px, 100% 100%, 0px 100%);"><div class="jarallax-img" style="background-image: url('/news-header.png'); object-fit: cover; object-position: 50% 50%; max-width: none; position: fixed; top: 0px; left: 0px; width: 1066px; height: 837.3px; overflow: hidden; pointer-events: none; transform-style: preserve-3d; backface-visibility: hidden; margin-top: 77.35px; transform: translate3d(0px, -50.75px, 0px);" data-jarallax-original-styles="background-image: url('/news-header.png');"></div>
+        </div>
+    </div>
 
     <nav class="container py-4 mb-2 my-lg-3" aria-label="breadcrumb">
         <ol class="breadcrumb mb-0">
