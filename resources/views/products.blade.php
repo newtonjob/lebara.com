de381a88585ebf812c4caa65b5a0ba87ceccea47
newtonjob <jobnewton3@gmail.com>
2025-07-12 09:03:16 +0100

    Use images for page headers

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,12 +1,7 @@
 <x-layouts.site>
-    <div class="jarallax d-none d-md-block" data-jarallax="" data-speed="0.35">
-        <span class="position-absolute top-0 start-0 w-100 h-100"></span>
-
-        <div class="d-none d-xxl-block" style="height: 600px;"></div>
-        <div class="d-none d-md-block d-xxl-none" style="height: 600px;"></div>
-        <div id="jarallax-container-0" class="jarallax-container" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden; z-index: -100; clip-path: polygon(0px 0px, 100% 0px, 100% 100%, 0px 100%);"><div class="jarallax-img" style="background-image: url('/products.png'); object-fit: cover; object-position: 50% 50%; max-width: none; position: fixed; top: 0px; left: 0px; width: 1066px; height: 837.3px; overflow: hidden; pointer-events: none; transform-style: preserve-3d; backface-visibility: hidden; margin-top: 77.35px; transform: translate3d(0px, -50.75px, 0px);" data-jarallax-original-styles="background-image: url('/products.png');"></div>
-        </div>
-    </div>
+    <section>
+        <img src="/products.png" alt="header">
+    </section>
 
     <nav class="container py-4 mb-2 my-lg-3" aria-label="breadcrumb">
         <ol class="breadcrumb mb-0">
